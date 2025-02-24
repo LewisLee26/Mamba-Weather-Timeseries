0efa7eed093991f7b4ac2684cb7a86fdb9e22fe7
LewisLee26 <lewis.patrick.lee@gmail.com>
2025-02-24 11:50:04 +0000

    Changed to gcs era5 data

--- download_data.py
+++ download_data.py
@@ -1,125 +1,103 @@
-import requests
-from tqdm import tqdm
-import datetime
-import pygrib
-import pandas as pd
+import xarray as xr
+import numpy as np
 import os
-
-def find_file_with_largest_prefix(directory):
-    # Step 1: Get a list of files in the target directory
-    files = os.listdir(directory)
-
-    if "gfs_dataframe.parquet" in files:
-        return "gfs_dataframe.parquet"
-
-    # Step 2 and 3: Extract prefixes and determine their lengths
-    prefixes_and_lengths = [(file, int(file[file.rfind('_')+1:file.rfind('.')])) for file in files]
-
-    # Step 4: Identify the file with the longest prefix
-    if prefixes_and_lengths:
-        max_prefix_file = max(prefixes_and_lengths, key=lambda x: x[1])[0]
-        return os.path.join(directory, max_prefix_file)
-    else:
-        return None
-
-# Example usage:
-result = find_file_with_largest_prefix("./")
-df = pd.read_parquet(result)
-
-# 2015, 1, 15 start date
-current_date = datetime.datetime(2015, 1, 15)
-end_date = datetime.datetime(2015, 6, 15)
-# current_year = datetime.datetime.today().year
-# current_month = datetime.datetime.today().month
-# current_day = datetime.datetime.today().day
-# end_date = datetime.datetime(current_year, current_month, current_day)
-
-coord_index = ((df['coord_index']).max() + 1) * 6
-current_date = current_date + datetime.timedelta(hours=coord_index)
-
-columns = [
-    "coord_index",
-    "lat",
-    "lon",
-    "Temperature",
-    "Surface pressure",
-    "V component of wind",
-    "U component of wind",
-    "Specific humidity",
-    "Convective precipitation",
-    "Total precipitation",
-    "Water equivalent of accumulated snow depth",
-    # "Soil Temperature",
-    # "Volumetric soil moisture content",
-]
-
-df = pd.DataFrame(columns=columns)
-
-pbar = tqdm(total = (1 * 30 * 4 * (10 * 20)))
+from tqdm import trange
+from time import time
+import argparse
 
 
+def log_time(func):
+    """Decorator to log the time taken by a function."""
 
-while True:
-    if end_date == current_date:
-        break
-    url = current_date.strftime(r"%Y/%Y%m%d/gfs.0p25.%Y%m%d%H.f003.grib2")
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        print(f"{func.__name__.replace('_', ' ').title()}: Processing", end="\r")
+        result = func(*args, **kwargs)
+        end_time = time()
+        print(
+            f"{func.__name__.replace('_', ' ').title()}: Complete - Time Taken: {end_time - start_time:.2f}"
+        )
+        return result
 
-    idx = url.rfind("/")
-    if (idx > 0):
-        ofile = url[idx+1:]
-    else:
-        ofile = url
+    return wrapper
 
-    try:
-        response = requests.get("https://data.rda.ucar.edu/ds084.1/" + url)
-    except Exception as e:
-        print(e)
-        break
 
-    with open(ofile, "wb") as f:
-        f.write(response.content)
-    f.close()
+@log_time
+def load_dataset():
+    return xr.open_zarr(
+        "gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr"
+    )
 
-    grbs = pygrib.open(ofile)
 
-    max_x = 10
-    max_y = 20
-    for x in range(max_x): # 721
-        for y in range(max_y):
-            lat = round((y/max_y)*720)
-            lon = round((x/max_x)*1439)
-            
-            df.loc[-1] = [
-                coord_index,
-                lat,
-                lon,
-                grbs[219].values[lat][lon], # Temperature
-                grbs[217].values[lat][lon], # Surface pressure
-                grbs[2].values[lat][lon], # V component of wind
-                grbs[1].values[lat][lon], # U component of wind
-                grbs[232].values[lat][lon], # Specific humidity
-                grbs[244].values[lat][lon], # Convective precipitation
-                grbs[243].values[lat][lon], # Total precipitation
-                grbs[228].values[lat][lon], # Water equivalent of accumlated snow depth
-                # grbs[226].values[lat][lon], # Soil temperature
-                # grbs[227].values[lat][lon], # Volumeric soil moisture content
-            ]  
+def prepare_directory(base_dir, date_str, time_str):
+    dir_path = os.path.join(base_dir, date_str, time_str)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    return dir_path
 
-            # shifting index
-            df.index = df.index + 1 
-            # sorting by index
-            df = df.sort_index()  
-            
-            pbar.update(1)
 
-    grbs.close()
+def main(start_date, end_date, base_dir):
+    ds = load_dataset()
 
-    checkpoint = 37
-    if coord_index % checkpoint == 0:
-        df.to_parquet(f"gfs_dataframe_{coord_index//checkpoint}.parquet")
+    ds_filtered = ds.sel(time=slice(start_date, end_date))
 
-    coord_index +=1
-    os.remove(ofile)
-    current_date += datetime.timedelta(hours=6)
+    progress_bar = trange(len(ds_filtered["time"]), desc="Processing time steps")
 
-df.to_parquet(f"gfs_dataframe.parquet")
+    for i in progress_bar:
+        current_time = ds_filtered["time"].isel(time=i).values
+        date_str = np.datetime_as_string(current_time, unit="D")
+        time_str = np.datetime_as_string(current_time, unit="m")[-5:]
+
+        progress_bar.set_description(f"Saving Data: {date_str} {time_str}")
+
+        dir_path = prepare_directory(base_dir, date_str, time_str)
+
+        surface_vars = [
+            "mean_sea_level_pressure",
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+            "2m_temperature",
+        ]
+        input_surface = np.stack(
+            [
+                ds_filtered[var].isel(time=i).values.astype(np.float32)
+                for var in surface_vars
+            ],
+            axis=0,
+        )
+
+        np.save(os.path.join(dir_path, "input_surface.npy"), input_surface)
+
+        upper_vars = [
+            "geopotential",
+            "specific_humidity",
+            "temperature",
+            "u_component_of_wind",
+            "v_component_of_wind",
+        ]
+        input_upper = np.stack(
+            [
+                ds_filtered[var].isel(time=i).values.astype(np.float32)
+                for var in upper_vars
+            ],
+            axis=0,
+        )
+
+        np.save(os.path.join(dir_path, "input_upper.npy"), input_upper)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process weather data.")
+    parser.add_argument(
+        "--start_date", type=str, required=True, help="Start date in YYYY-MM-DD format"
+    )
+    parser.add_argument(
+        "--end_date", type=str, required=True, help="End date in YYYY-MM-DD format"
+    )
+    parser.add_argument(
+        "--dir", type=str, default="data", help="Base directory for saving data"
+    )
+
+    args = parser.parse_args()
+
+    main(args.start_date, args.end_date, args.dir)
